327e8cb55dd410c1616cf592901e2e42254865bd
Kacepr Ostrowski <kacper.ostrowski777@gmail.com>
2023-03-24 22:29:32 +0100

    fixed bugs
    add: favourite bus stop

--- server/tabels/tabel.js
+++ server/tabels/tabel.js
@@ -11,15 +11,17 @@ module.exports = (req, res) => {
 				// console.log(results.Schedules.Stop[0].Day[0].R);
 				let buses = results.Schedules.Stop[0].Day[0].R;
 				let tableBuses = [];
-				buses.forEach((bus) => {
-					// console.log(bus.S[0].$);
-					tableBuses.push({
-						nr: bus.$.nr,
-						dir: bus.$.dir,
-						timeTo: bus.S[0].$.s,
-						timeAt: bus.S[0].$.t,
-					});
-				});
+				buses
+					? buses.forEach((bus) => {
+							// console.log(bus.S[0].$);
+							tableBuses.push({
+								nr: bus.$.nr,
+								dir: bus.$.dir,
+								timeTo: bus.S[0].$.s,
+								timeAt: bus.S[0].$.t,
+							});
+					  })
+					: null;
 				let data = {
 					time: results.Schedules.$.time,
 					busStopName: results.Schedules.Stop[0].$.name,
